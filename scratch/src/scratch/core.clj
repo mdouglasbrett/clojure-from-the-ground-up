c0445732ac8b5f7e45086cbf689339e9e502776f
Michael Brett <mdouglasbrett@icloud.com>
2017-12-08 08:38:05 +0000

    Remove foo

--- scratch/src/scratch/core.clj
+++ scratch/src/scratch/core.clj
@@ -4,8 +4,3 @@
   "Raises base to the given power. For instance, (pow 3 2) returns three squared, or nine"
   [base power]
   (apply * (repeat power base)))
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
