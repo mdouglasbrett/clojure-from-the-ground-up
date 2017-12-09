6526eed709d5e91a298563aad3ba4549e2a325e8
Michael Brett <mdouglasbrett@icloud.com>
2017-12-09 13:35:06 +0000

    Add most-duis function

--- scratch/src/scratch/crime.clj
+++ scratch/src/scratch/crime.clj
@@ -6,6 +6,17 @@
   [file]
   (json/parse-string (slurp file) true))
 
+(defn most-duis
+  "given a JSON filename of UCR crrime data for a particular year, finds the counties with the most DUIs."
+  [file]
+  (->> file
+       load-json
+       (sort-by :driving_under_influence)
+       (take-last 10)
+       (map #(select-keys % [:driving_under_influence
+                             :fips_county_code
+                             :fips_state_code]))))
+
 (def fips
   "A map of FIPS codes to their county names."
   (->> "fips.json"
