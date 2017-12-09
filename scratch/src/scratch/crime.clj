a682b9fec64c8d8889dde0d2eaf0fbbac7c69319
Michael Brett <mdouglasbrett@icloud.com>
2017-12-09 13:55:04 +0000

    Restructure code (fips in TDZ)

--- scratch/src/scratch/crime.clj
+++ scratch/src/scratch/crime.clj
@@ -6,23 +6,6 @@
   [file]
   (json/parse-string (slurp file) true))
 
-(defn most-duis
-  "Given a JSON filename of UCR crime data for a particular year, finds the counties with the most DUIs."
-  [file]
-  (->> file
-       load-json
-       (sort-by :driving_under_influence)
-       (take-last 10)
-       (map #(select-keys % [:driving_under_influence
-                             :fips_county_code
-                             :fips_state_code]))))
-
-(defn fips-code
-  "Given a county (a map with :fips_state_code and :fips_county_code keys), returns the five-digit FIPS code for the
-  county, as a string."
-  [county]
-  (str (:fips_state_code county) (:fips_county_code county)))
-
 (def fips
   "A map of FIPS codes to their county names."
   (->> "fips.json"
@@ -30,4 +13,24 @@
        :table
        :rows
        (into {})))
+
+(defn fips-code
+  "Given a county (a map with :fips_state_code and :fips_county_code keys), returns the five-digit FIPS code for the
+  county, as a string."
+  [county]
+  (str (:fips_state_code county) (:fips_county_code county)))
+
+(defn most-duis
+  "Given a JSON filename of UCR crime data for a particular year, finds the counties with the most DUIs."
+  [file]
+  (->> file
+       load-json
+       (sort-by :driving_under_influence)
+       (take-last 10)
+       (map (fn [county]
+              [(fips (fips-code county))
+               (:driving_under_influence county)]))
+       (into {})))
+
+
 
