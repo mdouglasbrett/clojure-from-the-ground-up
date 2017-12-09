c94214d99c314ced3a55e7ebf800e993d00becf5
Michael Brett <mdouglasbrett@icloud.com>
2017-12-09 13:45:47 +0000

    Add test for fips-code function

--- scratch/src/scratch/crime.clj
+++ scratch/src/scratch/crime.clj
@@ -7,7 +7,7 @@
   (json/parse-string (slurp file) true))
 
 (defn most-duis
-  "given a JSON filename of UCR crrime data for a particular year, finds the counties with the most DUIs."
+  "Given a JSON filename of UCR crime data for a particular year, finds the counties with the most DUIs."
   [file]
   (->> file
        load-json
@@ -16,6 +16,12 @@
        (map #(select-keys % [:driving_under_influence
                              :fips_county_code
                              :fips_state_code]))))
+
+(defn fips-code
+  "Given a county (a map with :fips_state_code and :fips_county_code keys), returns the five-digit FIPS code for the
+  county, as a string."
+  [county]
+  (str (:fips_state_code county) (:fips_county_code county)))
 
 (def fips
   "A map of FIPS codes to their county names."
