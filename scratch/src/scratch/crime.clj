93e4127a416f66b0021f25eb7c909beed859df49
Michael Brett <mdouglasbrett@icloud.com>
2017-12-09 13:28:16 +0000

    Add load-json func, refactor fips

--- scratch/src/scratch/crime.clj
+++ scratch/src/scratch/crime.clj
@@ -1,11 +1,16 @@
 (ns scratch.crime
   (:require [cheshire.core :as json]))
 
+(defn load-json
+  "Given a filename, reads a JSON file and returns it, parsed, with keywords."
+  [file]
+  (json/parse-string (slurp file) true))
+
 (def fips
   "A map of FIPS codes to their county names."
-  (->> (json/parse-string (slurp "fips.json") true)
+  (->> "fips.json"
+       load-json
        :table
        :rows
        (into {})))
-
 
