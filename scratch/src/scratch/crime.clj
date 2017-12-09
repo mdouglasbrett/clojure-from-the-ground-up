134f7dc55d5d8c1267a1b285b7ffe8275498c9e9
Michael Brett <mdouglasbrett@icloud.com>
2017-12-09 13:15:06 +0000

    Add fips Var

--- scratch/src/scratch/crime.clj
+++ scratch/src/scratch/crime.clj
@@ -1,2 +1,11 @@
 (ns scratch.crime
   (:require [cheshire.core :as json]))
+
+(def fips
+  "A map of FIPS codes to their county names."
+  (->> (json/parse-string (slurp "fips.json") true)
+       :table
+       :rows
+       (into {})))
+
+
